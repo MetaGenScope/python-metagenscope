af530ec1a9ddfd11b55e9c164cec532e3a729a52
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2017-12-21 12:32:33 -0500

    Fixed hello tests.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 import pytest
 from click.testing import CliRunner
-from metagenscope_cli import cli
+from metagenscope_cli import hello
 
 
 @pytest.fixture
@@ -9,21 +9,21 @@ def runner():
 
 
 def test_cli(runner):
-    result = runner.invoke(cli.main)
+    result = runner.invoke(hello.hello)
     assert result.exit_code == 0
     assert not result.exception
     assert result.output.strip() == 'Hello, world.'
 
 
 def test_cli_with_option(runner):
-    result = runner.invoke(cli.main, ['--as-cowboy'])
+    result = runner.invoke(hello.hello, ['--as-cowboy'])
     assert not result.exception
     assert result.exit_code == 0
     assert result.output.strip() == 'Howdy, world.'
 
 
 def test_cli_with_arg(runner):
-    result = runner.invoke(cli.main, ['Benjamin'])
+    result = runner.invoke(hello.hello, ['Benjamin'])
     assert result.exit_code == 0
     assert not result.exception
     assert result.output.strip() == 'Hello, Benjamin.'
