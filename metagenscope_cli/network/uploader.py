da066b843abf484910f6d2c6aeb1820e4662c51b
David Danko <dcdanko@gmail.com>
2018-03-13 00:03:50 -0400

    logic for sample upload

--- metagenscope_cli/network/uploader.py
+++ metagenscope_cli/network/uploader.py
@@ -1,6 +1,7 @@
 from metagenscope_cli.config import config
 from .token_auth import TokenAuth
 import requests
+import datetime
 
 
 class UploadAuthenticationError(Exception):
@@ -26,31 +27,60 @@ class Uploader:
             self.auth = TokenAuth(auth)
             self.auth_warn = 'unknown_token'
 
+        self.sample_uuid_map = None
+
     def warnings(self):
         return self.auth_warn
 
     def supress_warnings(self):
         self.auth_warn = None
 
-    def upload_data(self, result_name, result_type, data):
+    def _cache_sample_uuid(self, sample_name, sample_uuid):
+        if self.sample_uuid_map is None:
+            self._cache_sample_uuids()
+        self.sample_uuid_map[sample_name] = sample_uuid
+
+    def _cache_sample_uuids(self):
+        assert False
+
+    def _get_sample_uuid(self, sample_name, strict=False):
+        if self.sample_uuid_map is None:
+            self._cache_sample_uuids()
+        try:
+            return self.sample_uuid_map[sample_name]
+        except KeyError:
+            if strict:
+                raise
+            return sample_name
+
+    def _upload_payload(self, endpoint, payload):
+        if self.auth_warn is not None:
+            raise UploadAuthenticationError(self.auth_warn)
+        response = requests.post(self.url,
+                                 headers=self.headers,
+                                 auth=self.auth,
+                                 json=payload)
+        response.raise_for_status()
+        return response
+
+    def upload_sample_result(self, sample_name, result_name, result_type, data):
+        sample_uuid = self._get_sample_uuid(sample_name, strict=True)
         payload = {
             'result_name': result_name,
             'tool_name': result_type,
             'data': data,
         }
-        return self.upload_payload(payload)
+        endpoint = f'/api/v1/samples/{sample_uuid}/{result_type}'
+        return self._upload_payload(endpoint, payload)
 
-    def upload_payload(self, payload):
-        if self.auth_warn is not None:
-            raise UploadAuthenticationError(self.auth_warn)
-        request = requests.post(self.url,
-                                headers=self.headers,
-                                auth=self.auth,
-                                json=payload)
-        return request
-
-    def create_sample(self, sample_name, result_names=[]):
-        assert False
+    def create_sample(self, sample_name, metadata={}):
+        payload = {"name": sample_name,
+                   "metadata": metadata
+                   }
+        response = self._upload_payload('/api/v1/samples', payload)
+        sample_uuid = response['uuid']
+        self._cache_sample_uuid(sample_name, sample_uuid)
+        return response
 
     def create_sample_group(self,
                             group_name,
