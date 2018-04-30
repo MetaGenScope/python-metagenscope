a2ac22815a1027a291de0d06606044e689bb1ae4
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-04-30 06:43:16 -0400

    More lint fixes.

--- metagenscope_cli/cli/utils.py
+++ metagenscope_cli/cli/utils.py
@@ -4,10 +4,8 @@ from datetime import datetime
 from functools import wraps
 
 import click
-import requests
 from requests.exceptions import HTTPError
 
-from metagenscope_cli.config import config
 from metagenscope_cli.network import Knex, Uploader
 from metagenscope_cli.network.token_auth import TokenAuth
 
@@ -69,34 +67,3 @@ def add_authorization():
             return command(uploader, *args, **kwargs)
         return wrapper
     return decorator
-
-
-def handle_uploader_warnings(uploader):
-    if uploader.warnings() == 'unknown_token':
-        if click.confirm('Store token for future use (overwrites existing)?'):
-            try:
-                config.set_token(str(uploader.auth))
-            except AttributeError:
-                config.set_token(str(uploader.knex.auth))
-            uploader.suppress_warnings()
-        elif click.confirm('Continue with provided token?', abort=True):
-            uploader.suppress_warnings()
-    elif uploader.warnings() == 'no_auth':
-        click.secho('Warning: Skipping authentication', fg='yellow')
-        uploader.suppress_warnings()
-    return uploader
-
-
-def handle_uploader_response(request, verbose=False):
-    if request.status_code == requests.codes['created']:
-        click.secho('Success: submitted result.', fg='green')
-    else:
-        error_message = 'Bad Response: {0} '.format(request.status_code)
-        click.secho(error_message, fg='red')
-        if verbose:
-            try:
-                click.secho(request.json())
-            except ValueError:
-                # Invalid JSON in response
-                for line in request.text.splitlines():
-                    click.secho("    {0}".format(line), fg='red')
