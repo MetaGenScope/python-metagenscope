18c9c53badc10b7eb8a2967f2d8506363f188981
Danko David <dcd3001@hippocampus.pbtech>
2018-05-01 08:43:30 -0400

    who knows

--- metagenscope_cli/network/knex.py
+++ metagenscope_cli/network/knex.py
@@ -23,10 +23,13 @@ class Knex(object):
     def post(self, endpoint, payload):
         """Perform authenticated POST request."""
         url = self.host + endpoint
-        response = requests.post(url,
-                                 headers=self.headers,
-                                 auth=self.auth,
-                                 json=payload)
+        if payload:
+            response = requests.post(url,
+                                     headers=self.headers,
+                                     auth=self.auth,
+                                     json=payload)
+        else:
+            response = requests.post(url, headers=self.headers, auth=self.auth)
         if response.status_code >= 400:
             print(response.content, file=stderr)
         response.raise_for_status()
