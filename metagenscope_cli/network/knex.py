65ae253f321686b8dec79d1297797f9be7dec254
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-05-13 14:51:30 -0400

    Clean up remaining linting errors.

--- metagenscope_cli/network/knex.py
+++ metagenscope_cli/network/knex.py
@@ -5,7 +5,7 @@ import requests
 from metagenscope_cli.constants import DEFAULT_HOST
 
 
-class Knex:
+class Knex(object):
     """Knex wraps MetaGenScope requests requiring authentication."""
 
     def __init__(self, token_auth, host=None, headers=None):
